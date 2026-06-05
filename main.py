88f713a013a20e972fa0427e78589543ad690406
John Denny <johndenny116@gmail.com>
2026-06-05 12:09:35 +0100

    Test the brightness control

--- main.py
+++ main.py
@@ -5,7 +5,16 @@ import time
 with VM116() as dmx:
 
     end = time.time() + 5
-
+    
     while time.time() < end:
         dmx.set_d65()
         time.sleep(0.05)
+
+    end = time.time() + 5
+    dmx.brightness = 0.2
+    while time.time() < end:
+        dmx.set_d65()
+        time.sleep(0.05)
+
+
+    
